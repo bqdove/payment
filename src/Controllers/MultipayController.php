9f4f6cf0f7c2232c41e4805c4ce55ebed9a2ce45
linxing <876034274@qq.com>
2017-05-22 19:37:34 +0800

    working

--- src/Controllers/MultipayController.php
+++ src/Controllers/MultipayController.php
@@ -7,3 +7,39 @@
  * @datetime 2017-05-22 16:26
  */
 namespace Notadd\Multipay\Controllers;
+use Notadd\Foundation\Routing\Abstracts\Controller;
+use Notadd\Multipay\Handlers\GetHandler;
+use Notadd\Multipay\Handlers\SetHandler;
+
+/**
+ * Class MultipayController.
+ */
+class MultipayController extends Controller
+{
+    /**
+     * Get handler.
+     *
+     * @param \Notadd\Alipay\Handlers\GetHandler $handler
+     *
+     * @return \Notadd\Foundation\Passport\Responses\ApiResponse|\Psr\Http\Message\ResponseInterface|\Zend\Diactoros\Response
+     */
+    public function get(GetHandler $handler)
+    {
+        return $handler->toResponse()->generateHttpResponse();
+    }
+
+    /**
+     * Set handler.
+     *
+     * @param \Notadd\Alipay\Handlers\SetHandler $handler
+     *
+     * @return \Notadd\Foundation\Passport\Responses\ApiResponse|\Psr\Http\Message\ResponseInterface|\Zend\Diactoros\Response
+     * @throws \Exception
+     */
+    public function set(SetHandler $handler)
+    {
+        return $handler->toResponse()->generateHttpResponse();
+    }
+}
+
+
