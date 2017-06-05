57dd92acf041607c99c7708001641f428df317ac
linxing <linxing@ibenchu.com>
2017-06-05 17:04:28 +0800

    修改银联冲突

--- src/UnionPay.php
+++ src/UnionPay.php
@@ -11,27 +11,16 @@ namespace Notadd\Multipay;
 use Notadd\Foundation\Setting\Contracts\SettingsRepository;
 use Illuminate\Container\Container;
 use Omnipay\Omnipay;
-<<<<<<< HEAD
-=======
-
->>>>>>> 14d478de30da9ed924a313ee434c8b8dbc1a5f2f
 
 class UnionPay
 {
     protected $settings;
-<<<<<<< HEAD
-    protected $gateway;
-
-=======
-
     protected $gateway;
     //获取配置
->>>>>>> 14d478de30da9ed924a313ee434c8b8dbc1a5f2f
     public function __construct(){
         $this->settings = Container::getInstance()->make(SettingsRepository::class);
     }
 
-<<<<<<< HEAD
     public function getGateWay($gatewayname)
     {
             $gateway = Omnipay::create($gatewayname);
@@ -41,17 +30,7 @@ class UnionPay
             $gateway->setCertDir($this->settings->get('union.certDir'));
             $gateway->setReturnUrl($this->settings->get('union.returnUrl'));
             $gateway->setNotifyUrl($this->settings->get('union.notifyUrl'));
-=======
-    public function getGateway($gateway)
-    {
-            $this->gateway = Omnipay::create($gateway);
-            $this->gateway->setMerId($this->settings->get('union.merId'));
-            $this->gateway->setCertPath($this->settings->get('union.certPath'));
-            $this->gateway->setCertPassword($this->settings->get('union.certPassword'));
-            $this->gateway->setCertDir($this->settings->get('union.certDir'));
-            $this->gateway->setReturnUrl($this->settings->get('union.returnUrl'));
-            $this->gateway->setNotifyUrl($this->settings->get('union.notifyUrl'));
->>>>>>> 14d478de30da9ed924a313ee434c8b8dbc1a5f2f
+
 
             return $this;
     }
@@ -67,24 +46,13 @@ class UnionPay
                     'transType' => $transType// transtype
             ];
 
-<<<<<<< HEAD
-            $response = $gateway->purchase($order)->send();
-=======
             $response = $this->gateway->purchase($order)->send();
->>>>>>> 14d478de30da9ed924a313ee434c8b8dbc1a5f2f
 
             $response->getRedirectHtml(); //For PC/Wap
     }
 
-    public function webNotify()
+    public function webNotify($orderDesc,$orderId,$txnTime)
     {
-<<<<<<< HEAD
-            $gateway->setMerId($this->settings->get('union.merId'));
-=======
-            $this->gateway->setMerId($this->settings->get('union.merId'));
->>>>>>> 14d478de30da9ed924a313ee434c8b8dbc1a5f2f
-
-            $this->gateway->setCertDir($this->settings->get('union.certDir')); //The directory contain *.cer files
 
             $response = $this->gateway->completePurchase(['request_params'=>$_REQUEST])->send();
 
@@ -99,22 +67,15 @@ class UnionPay
       */
     public function query($merId, $transType, $orderId, $orderTime)
     {
-            $order = [
-                    'merId' => $merId,//merId
-                    'transType' => $transType,// transtype
-                    'orderId'   => $orderId, //Your order ID
-                    'orderTime'   => $orderTime, //Should be format 'YmdHis'
-            ];
-
-<<<<<<< HEAD
-
-            $response = $gateway->query($order)->send();
-            $response->isSuccessful();
-=======
-            $response = $this->gateway->query($order)->send();
->>>>>>> 14d478de30da9ed924a313ee434c8b8dbc1a5f2f
+        $order = [
+            'merId' => $merId,//merId
+            'transType' => $transType,// transtype
+            'orderId' => $orderId, //Your order ID
+            'orderTime' => $orderTime, //Should be format 'YmdHis'
+        ];
+        $response = $gateway->query($order)->send();
+        $response->isSuccessful();
     }
-
     /**
     *
     *退款接口
