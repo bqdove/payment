e44bfdf780560421f93ee5cac25bff97184c7239
linxing1126 <876034274@qq.com>
2017-06-10 10:54:26 +0800

    删除test路由修改回调路由名称

--- src/Listeners/RouteRegister.php
+++ src/Listeners/RouteRegister.php
@@ -24,15 +24,14 @@ class RouteRegister extends AbstractRouteRegister
             //http://pay.ibenchu.xyz:8080/pay?gate_way=wechat&way=Alipay_Express&money=100&sign=RSA2
 
         $this->router->group(['middleware' => ['cross', 'web'], 'prefix' => 'api/multipay'], function () {
-            $this->router->post('pay', PayController::class. '@pay');
+            $this->router->get('pay', PayController::class. '@pay');
             $this->router->get('query', PayController::class. '@query');
             $this->router->get('refund', PayController::class. '@refund');
             $this->router->get('cancel', PayController::class. '@cancel');
 
-            $this->router->get('test',PayController::class. '@test');
         });
         $this->router->get('upload', UploadController::class. '@upload');
-        $this->router->get('webnotice', PayController::class. '@webNotice');
+        $this->router->get('webnotify', PayController::class. '@webNotify');
         $this->router->post('execute', UploadController::class. '@execute');
 
     }
