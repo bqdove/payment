0778381d20b7cd23124cf2841b64f64c93802420
linxing1126 <876034274@qq.com>
2017-06-14 15:36:21 +0800

    增加payhandler

--- src/Handlers/PayHandler.php
+++ src/Handlers/PayHandler.php
@@ -8,8 +8,40 @@
 
 namespace Notadd\Multipay\Handlers;
 
+use Notadd\Foundation\Routing\Abstracts\Handler;
+use Illuminate\Container\Container;
+use Notadd\Foundation\Setting\Contracts\SettingsRepository;
 
-class PayHandler
+/*
+ * Classs PayHandler
+ */
+class PayHandler extends Handler
 {
+    /**
+     * @var SettingsRepository
+     */
+    protected $settings;
+
+    /**
+     * @var \Notadd\Multipay\Multipay
+     */
+    protected $multipay;
+
+    public function __construct(Container $container)
+    {
+        parent::__construct($container);
+        $this->multipay = $this->container->make('Multipay');
+        $this->settings = Container::getInstance()->make(SettingsRepository::class);
+    }
+
+    /*
+     * Execute Handler
+     */
+    public function execute(){
+        $driver = $this->request->query('driver');
+        $way = $this->request->query('way');
+        $para = $this->request->except(['driver','way']);
+        $this->multipay->query($driver,$way,$para);
+    }
 
 }
