5040cf9738eae67ef337db070c9a58f92649a6ef
谷祖楠 <13793165851@163.com>
2017-06-20 22:24:09 +0800

    调整路由

--- src/Listeners/RouteRegister.php
+++ src/Listeners/RouteRegister.php
@@ -42,13 +42,13 @@ class RouteRegister extends AbstractRouteRegister
             $this->router->group(['middleware' => ['cross', 'web'], 'prefix' => 'alipay'], function () {
                 $this->router->post('set',AlipayController::class.'@set');
                 $this->router->post('get',AlipayController::class.'@get');
-                $this->router->post('webnotify',AlipayController::class. '@webnotify');
+                $this->router->post('webnotify',AlipayController::class. '@webNotify');
             });
 
             $this->router->group(['middleware' => ['cross', 'web'], 'prefix' => 'wechat'], function () {
                 $this->router->post('set',WechatController::class.'@set');
                 $this->router->post('get',WechatController::class.'@get');
-                $this->router->post('webnotify',WechatController::class. '@webnotify');
+                $this->router->post('webnotify',WechatController::class. '@webNotify');
             });
 
             $this->router->group(['middleware' => ['cross', 'web'], 'prefix' => 'union'], function () {
