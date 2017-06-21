beeff0624fbeb65276e8d9e1a70b039f8305400f
谷祖楠 <13793165851@163.com>
2017-06-21 23:39:39 +0800

    修改

--- src/Controllers/AlipayController.php
+++ src/Controllers/AlipayController.php
@@ -40,8 +40,24 @@ class AlipayController extends Controller
      * @return \Notadd\Foundation\Passport\Responses\ApiResponse|\Psr\Http\Message\ResponseInterface|\Zend\Diactoros\Response
      * @throws \Exception
      */
-    public function set(SetAlipayconfHandler $handler)
+
+    public function set(SetWechatconfHandler $handler,Request $request)
     {
+        $this->validate($request,[
+            'app_id'=>'required|regex:/\w{18}/',
+            'mch_id'=>'required|regex:/\d{10}/',
+            'key'=>'required|regex:/\w{32}/',
+            'app_secret'=>'required|regex:/\w/',
+            'cert'=>'required|mimes:pem',
+            'cert_key'=>'required|mimes:pem'
+        ],[
+            'app_id'=>'app_id不能为空',
+            'mch_id'=>'mch_id不能为空',
+            'key'=>'key不能为空',
+            'app_secret'=>'app_secret不能为空',
+            'cert'=>'证书不能为空，证书必须为pem格式的',
+            'cert_key'=>'证书不能为空，证书必须为pem格式'
+        ]);
         return $handler->toResponse()->generateHttpResponse();
     }
 
