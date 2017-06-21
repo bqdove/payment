1931749ecadc0e920b1ddb0f9bb702cdf4e26baa
linxing1126 <876034274@qq.com>
2017-06-21 10:39:23 +0800

    删除路由文件中的测试路由

--- src/Listeners/RouteRegister.php
+++ src/Listeners/RouteRegister.php
@@ -35,28 +35,25 @@ class RouteRegister extends AbstractRouteRegister
             $this->router->get('test', PayController::class. '@test');
             $this->router->post('order', QueryController::class.'@orderList');
             $this->router->get('order',QueryController::class. '@all');
-            $this->router->get('test',UploadController::class.'@upload');
             $this->router->post('upload', UploadController::class. '@execute');
 
             $this->router->group(['middleware' => ['cross', 'web'], 'prefix' => 'alipay'], function () {
                 $this->router->post('set',AlipayController::class.'@set');
                 $this->router->post('get',AlipayController::class.'@get');
-                $this->router->post('webnotify',AlipayController::class. '@webnotify');
+                $this->router->post('webnotify',AlipayController::class. '@webNotify');
             });
 
             $this->router->group(['middleware' => ['cross', 'web'], 'prefix' => 'wechat'], function () {
                 $this->router->post('set',WechatController::class.'@set');
                 $this->router->post('get',WechatController::class.'@get');
-                $this->router->post('webnotify',WechatController::class. '@webnotify');
+                $this->router->post('webnotify',WechatController::class. '@webNotify');
             });
 
             $this->router->group(['middleware' => ['cross', 'web'], 'prefix' => 'union'], function () {
                 $this->router->post('set',UnionController::class.'@set');
                 $this->router->post('get',UnionController::class.'@get');
-                $this->router->post('webnotify',UnionController::class.'webnotify');
+                $this->router->post('webnotify',UnionController::class.'@webNotify');
             });
-
-            $this->router->any('webnotify',PayController::class. '@webNotify');
         });
     }
 }
