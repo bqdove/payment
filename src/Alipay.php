76f1711a3107558f436ff38aa4e8b4e3b6cdb105
谷祖楠 <13793165851@163.com>
2017-06-21 17:25:42 +0800

    完成支付宝异步回调存储数据操作

--- src/Alipay.php
+++ src/Alipay.php
@@ -46,7 +46,7 @@ class Alipay
         $this->gateway->setPrivateKey($this->settings->get('alipay.private_key'));//支付宝应用私钥
         $this->gateway->setAlipayPublicKey($this->settings->get('alipay.public_key'));//支付宝应用公钥
 //        $this->gateway->setSellerEmail($this->settings['seller_email']); //收款账户 email地址
-        $this->gateway->setReturnUrl('http://pay.ibenchu.xyz:8080/api/multipay/alipay/webnotify');
+        $this->gateway->setReturnUrl('http://pay.ibenchu.xyz:8080/');
         $this->gateway->setNotifyUrl('http://pay.ibenchu.xyz:8080/api/multipay/alipay/webnotify');
         $this->gateway->sandbox();
         return $this;
@@ -91,35 +91,32 @@ class Alipay
      */
     public function webNotify()
     {
-        Log::info(array_merge($_POST, $_GET));
-        $arrayData = array_merge($_POST, $_GET);
+        $arrayData = array_merge($_POST);
         $gateway = Omnipay::create('Alipay_AopPage');
         $gateway->setSignType($this->settings->get('alipay.sign_type')); // RSA/RSA2/MD5
         $gateway->setAppId($this->settings->get('alipay.app_id')); //支付宝应用ID
         $gateway->setPrivateKey($this->settings->get('alipay.private_key'));//支付宝应用私钥
-        $gateway->setAlipayPublicKey($this->settings->get('alipay.public_key'));//支付宝应用公钥
+        $gateway->setAlipayPublicKey('MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAsKgDn9nrnnpfQbhu7AG55wADL+3plJOtD9ty7i2c2FxgCNvhRSiKRHuv0C1yABI47H2dkl180frRWLsVWhy1RZ/F+xQwj2VK+0HZu8eAuwn1/rGdogLRFSIzWiYTd6d24QLdpzdtKbw9w5LJfRLnfu2nhLNkexXCasR+BdyurP0XCiqSu75IXNU4ZhXX338fhh+lQbHS4DVczz9SrhTNhhegk9VJgLdqvAIffnE9NSSE00onweDlPe6Ygsfxdtb0BHKgcBKHrhQTeYL1vvHID1zPBZQfs/JTDn4CfE130IiTyPVyRyH5SJijcMrue6EkYgARNwADLMu+DG8IzrCx6QIDAQAB');//支付宝应用公钥
 
         /**
          * @var AopCompletePurchaseResponse $response
          */
         try {
-            $response = $gateway->completePurchase(['params' => array_merge($_POST, $_GET)])->send();
+            $response = $gateway->completePurchase(['params' => array_merge($_POST)])->send();
 
             if ($response->isPaid()) {
                 /**
                  * Payment is successful
                  */
-                if($order = Order::where('out_trade_no', $arrayData['out_trade_no'])->first())
+                if($order = Order::where('out_trade_no', $_POST['out_trade_no'])->first())
                 {
-                    $order->out_trade_no = $arrayData['out_trade_no'];
-                    $order->total_amount = $arrayData['total_amount'];
-                    $order->trade_no = $arrayData['trade_no'];
-                    $order->seller_id = $arrayData['seller_id'];
+                    $order->total_amount = $_POST['total_amount'];
+                    $order->trade_no = $_POST['trade_no'];
+                    $order->seller_id = $_POST['seller_id'];
                     $order->trade_status = 1;
                     $order->payment = 'alipay';
-                    $order->created_at = $arrayData['timestamp'];
-                    $order->subject = 'phone';
-                    $order->pay_way = 'test';
+                    $order->created_at = $_POST['gmt_create'];
+                    $order->subject = $_POST['subject'];
                     $order->save();
                     die('success'); //The notify response should be 'success' only
                 }
