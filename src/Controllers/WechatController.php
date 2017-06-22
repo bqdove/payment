058b9cca5262c4ed4b198ac82f1936053804480a
谷祖楠 <13793165851@163.com>
2017-06-22 14:37:21 +0800

    修改app_id规则，应为18位字符串

--- src/Controllers/WechatController.php
+++ src/Controllers/WechatController.php
@@ -43,15 +43,15 @@ class WechatController extends Controller
     public function set(SetWechatconfHandler $handler,Request $request)
     {
         $this->validate($request,[
-            'app_id'=>'required|regex:/\w{16}/',
+            'app_id'=>'required|regex:/\w{18}/',
             'mch_id'=>'required|regex:/\d{10}/',
             'key'=>'required|regex:/\w{32}/',
             'app_secret'=>'required|regex:/\w/',
-            'cert'=>'required|mimes:pem',
-            'cert_key'=>'required|mimes:pem'
+            'cert'=>'required',
+            'cert_key'=>'required'
         ],[
             'app_id.required'=>'app_id不能为空',
-            'app_id.regex'=>'appid必须为16位字符串',
+            'app_id.regex'=>'appid必须为18位字符串',
             'mch_id.required'=>'商户Id不能为空',
             'mch_id.regex'=>'商户Id必须为为10位数字',
             'key.required'=>'商户密钥不能为空',
@@ -59,9 +59,9 @@ class WechatController extends Controller
             'app_secret.regex'=>'app_secret必须为字符串格式',
             'app_secret.required'=>'app_secret不能为空',
             'cert.required'=>'证书不能为空',
-            'cert.mimes'=>'证书必须为pem格式的',
+//            'cert.mimes'=>'证书必须为pem格式的',
             'cert_key.required'=>'证书不能为空',
-            'cert_key.mimes'=>'证书必须为pem格式'
+//            'cert_key.mimes'=>'证书必须为pem格式'
         ]);
 
         return $handler->toResponse()->generateHttpResponse();
