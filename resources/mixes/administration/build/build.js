7a8f7a41d3334c9d7512ef18d911b6432d34d42d
twilroad <269044570@qq.com>
2017-07-25 12:04:59 +0800

    refactor building information, and rebuild assets

--- resources/mixes/administration/build/build.js
+++ resources/mixes/administration/build/build.js
@@ -2,16 +2,18 @@ require('./check-versions')();
 
 process.env.NODE_ENV = 'production';
 
-var ora = require('ora');
 var path = require('path');
 var chalk = require('chalk');
+var merge = require('webpack-merge');
 var shell = require('shelljs');
 var webpack = require('webpack');
 var config = require('../config');
-var webpackConfig = require('./webpack.prod.conf');
-
-var spinner = ora('building for production...');
-spinner.start();
+var buildWebpackConfig = require('./webpack.prod.conf');
+var webpackConfig = merge(buildWebpackConfig, {
+    plugins: [
+        new webpack.ProgressPlugin()
+    ]
+});
 
 var assetsPath = path.join(config.build.assetsRoot, config.build.assetsSubDirectory);
 shell.rm('-rf', assetsPath);
@@ -21,7 +23,6 @@ shell.cp('-R', 'static/*', assetsPath);
 shell.config.silent = false;
 
 webpack(webpackConfig, function (err, stats) {
-    spinner.stop();
     if (err) throw err;
     process.stdout.write(stats.toString({
             colors: true,
@@ -32,8 +33,4 @@ webpack(webpackConfig, function (err, stats) {
         }) + '\n\n');
 
     console.log(chalk.cyan('  Build complete.\n'));
-    console.log(chalk.yellow(
-        '  Tip: built files are meant to be served over an HTTP server.\n' +
-        '  Opening index.html over file:// won\'t work.\n'
-    ));
 });
